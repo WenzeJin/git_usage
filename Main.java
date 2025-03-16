5b192325b28f62e255881f125a38eb3978438296
WenzeJin <wenzejin2004@gmail.com>
2025-03-17 00:10:38 +0800

    bye

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static void greet() {
+        System.out.println("Goodbye, World!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        greet();
     }
 }
